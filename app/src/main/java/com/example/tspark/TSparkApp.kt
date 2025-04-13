f84c1e10e4be6fdaa49f303447515a46d3f5a0b8
Iliyan-Y <maximizebg@gmail.com>
2025-04-13 16:14:14 +0100

    Update app bar created

--- app/src/main/java/com/example/tspark/TSparkApp.kt
+++ app/src/main/java/com/example/tspark/TSparkApp.kt
@@ -1,21 +1,83 @@
 package com.example.tspark
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.tspark.ui.ChargeCalculatorScreen
 
+enum class AppScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Settings(title = R.string.settings),
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TSparkApp() {
+fun TSparkAppBar(
+    currentScreen: AppScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun TSparkApp(
+    navController: NavHostController = rememberNavController()
+) {
     val focusManager = LocalFocusManager.current
+    // Get current back stack entry
+    val backStackEntry = navController.currentBackStackEntryAsState().value
+    // Get the name of the current screen
+    val currentScreen = AppScreen.valueOf(
+        backStackEntry?.destination?.route ?: AppScreen.Start.name
+    )
     Scaffold(
+        topBar = {
+            TSparkAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        },
         modifier = Modifier
             .fillMaxSize()
             .pointerInput(Unit) {
