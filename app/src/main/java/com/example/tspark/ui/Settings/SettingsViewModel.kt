2fdb5f9ea4a1d6f609b8ec49f2008d7ca24776f0
Iliyan-Y <maximizebg@gmail.com>
2025-04-21 09:52:27 +0100

    Created settings view model

--- app/src/main/java/com/example/tspark/ui/Settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/tspark/ui/Settings/SettingsViewModel.kt
@@ -1,8 +1,44 @@
 package com.example.tspark.ui.Settings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tspark.data.Settings
 import com.example.tspark.data.SettingsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+data class SettingsState(
+    val batteryCapacity: Double = 0.0,
+    val initialRange: Double = 0.0
+)
 
 class SettingsViewModel(private val settingsRepository: SettingsRepository) : ViewModel() {
-    //todo
+
+    val settingsUiState: StateFlow<SettingsState> =
+        settingsRepository.getSettingsStream()
+            .map { it.toSettingsState() }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = SettingsState()
+            )
+
+
+}
+
+/**
+ * Extension function to convert [Settings] to [SettingsState].
+ * It handles the case where the [Settings] object might be null.
+ */
+fun Settings?.toSettingsState(): SettingsState {
+    return if (this != null) {
+        SettingsState(
+            batteryCapacity = batteryCapacity,
+            initialRange = initialRange
+        )
+    } else {
+        SettingsState() // Return default state when settings is null
+    }
 }
